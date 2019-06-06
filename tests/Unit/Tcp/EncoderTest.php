29ba90161728f46f4dfc553985e9b46d073df086
Gerardo Rico Botella <gerardo.rico@multimediacreations.eu>
2019-06-06 18:25:14 +0200

    Fix encoder error

--- tests/Unit/Tcp/EncoderTest.php
+++ tests/Unit/Tcp/EncoderTest.php
@@ -13,7 +13,7 @@ class EncoderTest extends TestCase
     public function can_encode_acknowledge()
     {
         $packet = Mockery::mock(Acknowledgeable::class)
-                    ->shouldReceive('numberOfAcceptedData')
+                    ->shouldReceive('getNumberOfAcceptedData')
                     ->andReturns(2)->getMock();
 
         $ack = (new Encoder)->encodeAcknowledge($packet);
